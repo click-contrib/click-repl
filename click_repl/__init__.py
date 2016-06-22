61a29d986bfe97a1843b9f60910200c808b66a86
Stefan Scherfke <stefan@sofa-rockers.org>
2016-06-22 23:06:25 +0200

    Move cli function off the original scope to allow more flexible usage.

--- click_repl/__init__.py
+++ click_repl/__init__.py
@@ -15,11 +15,14 @@ _internal_commands = dict()
 
 
 def _register_internal_command(names, target, description=None):
-    assert hasattr(target, '__call__'), 'internal command must be a callable'
+    if not hasattr(target, '__call__'):
+        raise ValueError('Internal command must be a callable')
+
     if isinstance(names, six.string_types):
         names = [names]
     elif not isinstance(names, (list, tuple)):
         raise ValueError('"names" must be a string or a list / tuple')
+
     for name in names:
         _internal_commands[name] = (target, description)
 
@@ -106,66 +109,74 @@ class ClickCompleter(Completer):
                 yield item
 
 
+def repl(old_ctx):
+    """
+    Start an interactive shell. All subcommands are available in it.
+
+    You can also pipe to this command to execute subcommands.
+
+    """
+    # parent should be available, but we're not going to bother if not
+    group_ctx = old_ctx.parent or old_ctx
+    group = group_ctx.command
+    isatty = sys.stdin.isatty()
+    if isatty:
+        history = InMemoryHistory()
+        completer = ClickCompleter(group)
+
+        def get_command():
+            return prompt(u'> ', completer=completer, history=history)
+    else:
+        get_command = sys.stdin.readline
+
+    while True:
+        try:
+            command = get_command()
+        except KeyboardInterrupt:
+            continue
+        except EOFError:
+            break
+
+        if not command:
+            if isatty:
+                continue
+            else:
+                break
+
+        if dispatch_repl_commands(command):
+            continue
+
+        try:
+            result = handle_internal_commands(command)
+            if isinstance(result, six.string_types):
+                click.echo(result)
+                continue
+        except ExitReplException:
+            break
+
+        args = shlex.split(command)
+
+        try:
+            with group.make_context(None, args, parent=group_ctx) as ctx:
+                group.invoke(ctx)
+                ctx.exit()
+        except click.ClickException as e:
+            e.show()
+        except SystemExit:
+            pass
+
+
 def register_repl(group, name='repl'):
-    @group.command(name=name)
-    @click.pass_context
-    def cli(old_ctx):
-        """
-        Start an interactive shell. All subcommands are available in it.
-
-        You can also pipe to this command to execute subcommands.
-        """
-
-        # parent should be available, but we're not going to bother if not
-        group_ctx = old_ctx.parent or old_ctx
-        isatty = sys.stdin.isatty()
-        if isatty:
-            history = InMemoryHistory()
-            completer = ClickCompleter(group)
-
-            def get_command():
-                return prompt(u'> ', completer=completer, history=history)
-        else:
-            get_command = sys.stdin.readline
-
-        while True:
-            try:
-                command = get_command()
-            except KeyboardInterrupt:
-                continue
-            except EOFError:
-                break
-
-            if not command:
-                if isatty:
-                    continue
-                else:
-                    break
-
-            if dispatch_repl_commands(command):
-                continue
-
-            try:
-                result = handle_internal_commands(command)
-                if isinstance(result, six.string_types):
-                    click.echo(result)
-                    continue
-            except ExitReplException:
-                break
-
-            args = shlex.split(command)
-
-            try:
-                with group.make_context(None, args, parent=group_ctx) as ctx:
-                    group.invoke(ctx)
-                    ctx.exit()
-            except click.ClickException as e:
-                e.show()
-            except SystemExit:
-                pass
+    """Register :func:`repl()` as sub-command *name* of *group*."""
+    group.command(name=name)(click.pass_context(repl))
 
 
 def dispatch_repl_commands(command):
+    """Execute system commands entered in the repl.
+
+    System commands are all commands starting with "!".
+
+    """
     if command.startswith('!'):
         os.system(command[1:])
         return True
@@ -174,6 +185,11 @@ def dispatch_repl_commands(command):
 
 
 def handle_internal_commands(command):
+    """Run repl-internal commands.
+
+    Repl-internal commands are all commands starting with ":".
+
+    """
     if command.startswith(':'):
         target = _get_registered_target(command[1:], default=None)
         if target:
