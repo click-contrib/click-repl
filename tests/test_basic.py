88343f711561c0e69564a59010070114b06d1d6d
Markus Unterwaditzer <markus@unterwaditzer.net>
2017-01-23 21:16:46 +0100

    Stylefix

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -2,21 +2,19 @@ import click
 from click_repl import ClickCompleter
 from prompt_toolkit.document import Document
 
+
 def test_completion():
     @click.group()
     def root_command():
         pass
 
-
     @root_command.group()
     def first_level_command():
         pass
 
-
     @first_level_command.command()
     def second_level_command_one():
         pass
-
 
     @first_level_command.command()
     def second_level_command_two():
