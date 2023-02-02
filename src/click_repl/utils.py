c187e3c3460ac4a6243a307b6275e4fbd9da2548
Ghost Ops <72981180+GhostOps77@users.noreply.github.com>
2023-02-02 17:33:49 +0530

    import future shifted to line 1

--- src/click_repl/utils.py
+++ src/click_repl/utils.py
@@ -1,3 +1,5 @@
+from __future__ import annotations, print_function
+
 import os
 import shlex
 import sys
@@ -12,7 +14,6 @@ from prompt_toolkit.completion import CompleteEvent, Completer, Completion
 from prompt_toolkit.document import Document
 from prompt_toolkit.history import InMemoryHistory
 
-from __future__ import annotations, print_function
 from .exceptions import ExitReplException, InternalCommandException  # noqa
 
 
