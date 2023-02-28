4f17d60d11f938ca55f0f4ad14eb8ea07b3c8d93
Ghost Ops <72981180+GhostOps77@users.noreply.github.com>
2023-02-28 20:29:01 +0530

    Actually added the help argument to the option
    Sorry for the inconvenience, I've forgot to add the help argument to the option decorator, but this commit includes it

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -24,7 +24,7 @@ def test_arg_completion():
 
 def test_option_completion():
     @root_command.command()
-    @click.option("--handler", "-h", type=click.Choice(("foo", "bar")))
+    @click.option("--handler", "-h", type=click.Choice(("foo", "bar")), help="Demo option")
     def option_cmd(handler):
         pass
 
