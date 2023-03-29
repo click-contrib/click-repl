369405d7bd536629653f5ddec516f179236ebd5a
Francis J. Lacoste <francis.lacoste@contre.com>
2023-03-28 21:04:01 -0400

    Added test showing that subcommands are dispatched, and two failing tests for issue #92.

--- tests/test_repl.py
+++ tests/test_repl.py
@@ -1,6 +1,21 @@
+import contextlib
+import sys
+
 import click
 import click_repl
 import pytest
+
+from io import StringIO
+
+
+@contextlib.contextmanager
+def mock_stdin(text):
+    old_stdin = sys.stdin
+    try:
+        sys.stdin = StringIO(text)
+        yield sys.stdin
+    finally:
+        sys.stdin = old_stdin
 
 
 def test_simple_repl():
@@ -21,7 +36,65 @@ def test_simple_repl():
     click_repl.register_repl(cli)
 
     with pytest.raises(SystemExit):
-        cli()
+        cli(args=[], prog_name="test_simple_repl")
+
+
+def test_repl_dispatches_subcommand(capsys):
+    @click.group(invoke_without_command=True)
+    @click.pass_context
+    def cli(ctx):
+        if ctx.invoked_subcommand is None:
+            click_repl.repl(ctx)
+
+    @cli.command()
+    def foo():
+        print("Foo!")
+
+    with mock_stdin("foo\n"):
+        with pytest.raises(SystemExit):
+            cli(args=[], prog_name="test_repl_dispatch_subcommand")
+    assert capsys.readouterr().out == "Foo!\n"
+
+
+def test_group_command_called_once(capsys):
+    @click.group(invoke_without_command=True)
+    @click.pass_context
+    def cli(ctx):
+        print("cli()")
+        if ctx.invoked_subcommand is None:
+            click_repl.repl(ctx)
+
+    @cli.command()
+    def foo():
+        print("Foo!")
+
+    @cli.command()
+    def bar():
+        print("Bar!")
+
+    with mock_stdin("foo\nbar\n"):
+        with pytest.raises(SystemExit):
+            cli(args=[], prog_name="test_group_called_once")
+    assert capsys.readouterr().out == "cli()\nFoo!\nBar!\n"
+
+
+def test_independant_args(capsys):
+    @click.group(invoke_without_command=True)
+    @click.argument("argument")
+    @click.pass_context
+    def cli(ctx, argument):
+        print("cli(%s)" % argument)
+        if ctx.invoked_subcommand is None:
+            click_repl.repl(ctx)
+
+    @cli.command()
+    def foo():
+        print("Foo!")
+
+    with mock_stdin("foo\n"):
+        with pytest.raises(SystemExit):
+            cli(args=["command-line-argument"], prog_name="test_group_called_once")
+    assert capsys.readouterr().out == "cli(command-line-argument)\nFoo!\n"
 
 
 def test_exit_repl_function():
@@ -41,7 +114,7 @@ def test_inputs(capfd):
         click_repl.repl(click.get_current_context())
 
     try:
-        cli()
+        cli(args=[], prog_name="test_inputs")
     except (SystemExit, Exception) as e:
         if (
             type(e).__name__ == "prompt_toolkit.output.win32.NoConsoleScreenBufferError"
