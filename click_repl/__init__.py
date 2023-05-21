6cd86c627a03a861a91a94322f11269d04b247c1
Ghost Ops <72981180+GhostOps77@users.noreply.github.com>
2023-05-21 18:21:49 +0530

    Removed the CommandLineParserError import statement

--- click_repl/__init__.py
+++ click_repl/__init__.py
@@ -1,7 +1,6 @@
 from ._completer import ClickCompleter as ClickCompleter  # noqa: F401
 from ._repl import register_repl as register_repl  # noqa: F401
 from ._repl import repl as repl  # noqa: F401
-from .exceptions import CommandLineParserError as CommandLineParserError  # noqa: F401
 from .exceptions import ExitReplException as ExitReplException  # noqa: F401
 from .exceptions import (  # noqa: F401
     InternalCommandException as InternalCommandException,
