2691b6dc05b021c0e2cda0b4bfb8c137dc92fbfc
Ghost Ops <72981180+GhostOps77@users.noreply.github.com>
2023-05-21 10:18:59 +0530

    Removed unnecessary explicit imports from click_repl/__init__.py

--- click_repl/__init__.py
+++ click_repl/__init__.py
@@ -1,11 +1,5 @@
-from ._completer import ClickCompleter as ClickCompleter  # noqa: F401
-from ._repl import register_repl as register_repl  # noqa: F401
-from ._repl import repl as repl  # noqa: F401
-from .exceptions import CommandLineParserError as CommandLineParserError  # noqa: F401
-from .exceptions import ExitReplException as ExitReplException  # noqa: F401
-from .exceptions import (  # noqa: F401
-    InternalCommandException as InternalCommandException,
-)
-from .utils import exit as exit  # noqa: F401
+from ._completer import ClickCompleter  # noqa: F401
+from ._repl import register_repl, repl  # noqa: F401
+from .utils import exit  # noqa: F401
 
 __version__ = "0.2.0"
