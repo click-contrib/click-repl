3239d97c90a3436c7cea0f69deca53db37175fec
Lucien A <lu.aubert84@gmail.com>
2023-06-21 18:47:00 +0200

    add unit test

--- tests/test_completion/test_common_tests/test_option_completion.py
+++ tests/test_completion/test_common_tests/test_option_completion.py
@@ -14,11 +14,15 @@ c = ClickCompleter(root_command, click.Context(root_command))
 def test_option_choices():
     @root_command.command()
     @click.option("--handler", type=click.Choice(("foo", "bar")))
+    @click.option("--wrong", type=click.Choice(("bogged", "bogus")))
     def option_choices(handler):
         pass
 
     completions = list(c.get_completions(Document("option-choices --handler ")))
     assert {x.text for x in completions} == {"foo", "bar"}
+
+    completions = list(c.get_completions(Document("option-choices --wrong ")))
+    assert {x.text for x in completions} == {"bogged", "bogus"}
 
 
 def test_boolean_option():
